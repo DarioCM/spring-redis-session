6cebef7639769f19e5663431531fb96a105bf0b3
Carlos D <dario20049@gmail.com>
2024-10-23 18:04:18 +0900

    Initial commit

---
+++ src/test/java/dev/dario/springredissession/SpringRedisSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dario.springredissession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
